1515a528d795811bc37e4210cb39401dfc13c7d0
ahn <Aprilannefedelino2021@gmail.com>
2026-05-23 13:28:00 +0800

    Add files via upload

---
+++ backend/api/urls.py
@@ -0,0 +1,15 @@
+# from django.urls import path
+# from .views import task_list
+
+# urlpatterns = [
+#     path('tasks/', task_list),
+# ]
+
+from django.urls import path
+from .views import task_list, task_detail
+
+
+urlpatterns = [
+    path('tasks/', task_list),
+    path('tasks/<int:pk>/', task_detail),
+]
